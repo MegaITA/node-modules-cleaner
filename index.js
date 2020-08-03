ef82238cb47bdf20e0bc6ba96f01e1ab9c0d6707
Alessandro Bertozzi <aleberto@live.it>
2020-08-03 20:41:29 +0200

    A bit of error handling and input checking

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const fs = require('fs');
 const rimraf = require('rimraf');
 const { getValueByParamName } = require('./utils/argsParser');
 
-const startingPath = fs.realpathSync(getValueByParamName('-path', process.argv));
+if(process.argv.length <= 2)
+  return console.log('You must specify a projects folder directory.\nCommand usage: modules-cleaner -path <path>')
+
+try {
+  
+  const startingPath = fs.realpathSync(getValueByParamName('-path', process.argv));
+
+} catch (error) {
+
+  if(error.code === 'ENOENT')
+    return console.log('The provided directory is not valid.');
+
+}
 
 const projectFolders = fs.readdirSync(startingPath, { withFileTypes: true })
   .filter(v => !v.isFile());
